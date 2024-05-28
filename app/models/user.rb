35f8b20fcf05098adca34201fe9191967fa87f02
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-28 14:29:54 +0900

    アカウントを編集、削除機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 20 }, format: { with: /\A[a-zA-Z0-9]+\z/, multiline: true, message: I18n.t('errors.user.name') }
+  validates :name, presence: true, length: { maximum: 20 }, format: { with: /\A[a-zA-Z0-9]+\z/, multiline: true, message: I18n.t('errors.user.name') },
+                   uniqueness: true
   validates :introduction, length: { maximum: 200 }
   validates :external_blog_url, length: { maximum: 2048 }
 
