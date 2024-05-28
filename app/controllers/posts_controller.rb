acf59f74aeec7de3b1195edca22a641cf340074d
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-28 15:58:38 +0900

    投稿一覧画面を作成する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,6 @@ class PostsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(10)
   end
 end
