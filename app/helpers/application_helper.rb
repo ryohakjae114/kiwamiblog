e300b20a894b5ab102591eb06c333587dc1613ca
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-28 11:35:38 +0900

    アカウント登録画面からアカウント登録ができる

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def title(title_text)
+    content_tag :div, title_text, class: 'fs-3 my-4'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 20 }, format: { with: /^[0-9a-zA-Z]+$/, multiline: true, message: I18n.t('errors.user.name') }
+  validates :name, presence: true, length: { maximum: 20 }, format: { with: /\A[a-zA-Z0-9]+\z/, multiline: true, message: I18n.t('errors.user.name') }
   validates :introduction, length: { maximum: 200 }
   validates :external_blog_url, length: { maximum: 2048 }
 
