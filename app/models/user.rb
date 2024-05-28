d60274901df087f1e32c4698f4380be6add6d00a
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-28 11:00:53 +0900

    nameとpasswordでユーザ認証する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 20 }, format: { with: /^[0-9a-zA-Z]+$/, message: t('errors.user.name') }
+  validates :name, presence: true, length: { maximum: 20 }, format: { with: /^[0-9a-zA-Z]+$/, multiline: true, message: I18n.t('errors.user.name') }
   validates :introduction, length: { maximum: 200 }
   validates :external_blog_url, length: { maximum: 2048 }
+
+  def email_required?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end
