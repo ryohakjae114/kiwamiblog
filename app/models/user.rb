fbc126b2228a0c72357b0147da42ffb054dc289c
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-29 13:47:00 +0900

    inver_ofオプションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
-  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'following_id', dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'following_id', dependent: :destroy, inverse_of: :following
   has_many :followings, through: :active_relationships, source: :followed
 
-  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy, inverse_of: :followed
   has_many :followers, through: :passive_relationships, source: :followed
 
   def follow?(user)
