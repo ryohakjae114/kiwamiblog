e1f447a41fcb56c28ca6fd8d790fbcd896071bf2
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-29 13:31:37 +0900

    userのnameをdb層でuniqueに

---
+++ db/migrate/20240529043007_add_uniqueness_name_to_users.rb
@@ -0,0 +1,5 @@
+class AddUniquenessNameToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_index :users, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_005725) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_043007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_005725) do
     t.text "external_blog_url", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "posts", "users"
