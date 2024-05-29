20032c8850649886069378c1082f91304f812428
ryohakjae114 <61152037+ryohakjae114@users.noreply.github.com>
2024-05-29 10:40:47 +0900

    ユーザのフォローとフォロー解除ができる

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,16 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'following_id', dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :followed
+
+  def follow!(user)
+    active_relationships.create!(followed_id: user.id) unless self == user
+  end
+
   def email_required?
     false
   end
